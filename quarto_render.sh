25bd356cf90f376d0b73fbe1c7228df76dd24c09
Armin Penz <Armin.Penz@student.uibk.ac.at>
2026-02-24 12:29:47 +0100

    rename script to render with quarto

--- quarto_render.sh
+++ quarto_render.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-module purge 
-module load miniconda/tci-collection-v1   
+module purge
+module load miniconda/tci-collection-v1
 
-# if conda env is not already created, create it
 if [ ! -d $HOME/.conda/envs/quarto_env ]; then
     conda env create -f environment.yml
 fi
